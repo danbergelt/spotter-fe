4931e93b2af8ad4003cf0703f322ccc2d7b81046
Dan Bergelt <dan@danbergelt.com>
2020-03-03 14:18:00 -0500

    :white_check_mark: change password integration tests
    change password integration tests

--- src/__testUtils__/types.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosStatic } from 'axios';
-
-export interface AxiosMock extends AxiosStatic {
-  mockResolvedValue: Function;
-  mockRejectedValue: Function;
-  mockClear: Function;
-}
--- src/__tests__/unit/components/Label.test.tsx
+++ src/__tests__/unit/components/Label.test.tsx
@@ -5,15 +5,12 @@ import Label from 'src/components/util/Label';
 describe('Label utility component', () => {
   test('can render label', () => {
     const { queryByText } = render(<Label content='foo' input='bar' />);
-
     expect(queryByText(/foo/i)).toBeTruthy();
   });
 
   test('pairs label with input passed as prop', () => {
     const { getByText } = render(<Label content='foo' input='bar' />);
-
     const label = getByText(/foo/i) as HTMLLabelElement;
-
     expect(label.htmlFor).toEqual('bar');
   });
 });
