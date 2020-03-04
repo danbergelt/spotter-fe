c3cc23df73be276da4afd5460e8c2458c0465a72
Dan Bergelt <dan@danbergelt.com>
2020-03-03 21:37:03 -0500

    :pencil: commenting contact components
    commenting contact components

--- src/components/contact/Popup.tsx
+++ src/components/contact/Popup.tsx
@@ -1,37 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import { ReactComponent as Chat } from '../../assets/contact_icon_1.svg';
 import { FiX } from 'react-icons/fi';
-import useDelayUnmount from './useDelayUnmount';
+import useDelayUnmount from '../../hooks/useDelayUnmount';
 import { useLocation } from 'react-router-dom';
 import 'animate.css';
 import ContactForm from './ContactForm';
 import ScrollLock from 'react-scrolllock';
 import { useWindowSize } from 'react-use';
 
-/*
-  Contact pop-up
-  Renders a form that allows the user to send an email w/ questions, concerns, etc.
-  Displays on all pages in a fixed position in bottom-right corner
-  Inspired by services such as Intercom
+/*== Contact popup =====================================================
+
+The popup component that renders the contact form. Appears as a small
+circle on bottom right hand side of all pages, allows users (auth'd or
+not) to send a contact message to the service team at spotter. 
+
+The design is inspired by enterprise chat support systems such as Intercom:
+https://www.intercom.com/
+
 */
 
 const Popup: React.FC = () => {
+  // form state, determines if contact form is shown
   const [form, setForm] = useState<boolean>(false);
+
+  // a custom hook that delay's the unmounting of a component,
+  // allowing us to run unmount transition animations
   const customMount: boolean = useDelayUnmount(form, 500);
+
+  // grab the pathname, close the form
+  // whenever we travel to a new page
   const { pathname } = useLocation();
 
+  // animation strings from animate.css https://github.com/daneden/animate.css/
   const close = 'animated rotateIn faster';
   const open = 'animated zoomIn faster';
 
+  // close the form when the page changes
   useEffect(() => {
     setForm(false);
   }, [pathname]);
 
+  // grab the page width for scroll locking
   const { width } = useWindowSize();
 
   return (
     <>
+      {/* lock scrolling when contact form is open on mobile */}
       <ScrollLock isActive={width <= 450 && form} />
+
+      {/* run the transitions, then toggle the form */}
       {customMount && <ContactForm form={form} />}
       <div
         data-testid='contact-button'
@@ -39,6 +56,7 @@ const Popup: React.FC = () => {
         onClick={(): void => setForm(!form)}
         className='contact-popup-button'
       >
+        {/* render X icon if form is open */}
         {form && (
           <FiX
             size='27.5px'
@@ -46,6 +64,7 @@ const Popup: React.FC = () => {
           />
         )}
 
+        {/* render chat icon if form is closed */}
         {!form && (
           <Chat className={!form ? `${open} contact-open` : 'contact-open'} />
         )}
