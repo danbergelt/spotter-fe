a46a160a2001c7e0d919aa1181c4ba0a860d4331
Dan Bergelt <dan@danbergelt.com>
2020-03-03 19:00:19 -0500

    :pencil: authroute comments
    authroute comments

--- src/components/auth/AuthRoute.tsx
+++ src/components/auth/AuthRoute.tsx
@@ -2,12 +2,31 @@ import React, { ReactNode } from 'react';
 import { RouteProps, Redirect, Route as Router } from 'react-router-dom';
 import useToken from 'src/hooks/useToken';
 
-// custom route that includes authentication check
-// pushes users around site according to their auth status
+/*== Auth route =====================================================
 
-// auth === null && universally accessible
-// auth === true && logged-in users only
-// auth === false && non logged-in users only
+Custom route that extends React Router's Route component. Adds
+functionality that performs auth checking and automatic redirects.
+Pushes users around site according to their auth status
+
+Can also function like a default React Router Route by accepting null
+in the auth prop
+
+Rules:
+  auth === null && universally accessible
+  auth === true && logged-in users only
+  auth === false && non logged-in users only
+
+Props:
+  component: React Component
+    The component to push the user to
+  auth: boolean | null
+    Whether the route is protected agains auth'd users, 
+    protected against unauth'd users, or open to all users
+  
+  Also accepts additional react router props such as "exact",
+  whch are then spread into the component
+
+*/
 
 interface Props extends RouteProps {
   component: React.ComponentType<RouteProps>;
