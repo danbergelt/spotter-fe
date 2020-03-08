7cbaec80e2579cc0c315d2de94b08f8c627039b8
Dan Bergelt <dan@danbergelt.com>
2020-03-08 01:01:52 -0500

    :fire: removed sticky nav
    removed sticky nav

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,18 +1,16 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { useWindowSize, useWindowScroll } from 'react-use';
+import { useWindowSize } from 'react-use';
 import NavLinks from './NavLinks';
 import { FiMenu, FiX } from 'react-icons/fi';
 import Flex from '../util/Flex';
 import styles from './Nav.module.scss';
 import Dropdown from '../util/Dropdown';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Nav: React.FC = () => {
   const { width } = useWindowSize();
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef<HTMLSpanElement>(null);
-  const { pathname } = useLocation();
-  const { y } = useWindowScroll();
 
   useEffect(() => {
     if (width > 500) {
@@ -51,27 +49,17 @@ const Nav: React.FC = () => {
     }
   };
 
-  const checkPath = (): boolean => pathname === '/';
-
   return (
-    <div
-      style={{
-        position: checkPath() ? 'fixed' : undefined,
-        width: checkPath() ? '100%' : undefined
-      }}
-      className={checkPath() && y > 50 ? styles.scrolling : undefined}
+    <Flex
+      justify='space-between'
+      align={width <= 500 ? 'flex-end' : undefined}
+      cn={styles.container}
     >
-      <Flex
-        justify='space-between'
-        align={width <= 500 ? 'flex-end' : undefined}
-        cn={styles.container}
-      >
-        <Link className={styles.logo} data-testid='spotter' to={'/'}>
-          spotter
-        </Link>
-        {renderLinks()}
-      </Flex>
-    </div>
+      <Link className={styles.logo} data-testid='spotter' to={'/'}>
+        spotter
+      </Link>
+      {renderLinks()}
+    </Flex>
   );
 };
 
