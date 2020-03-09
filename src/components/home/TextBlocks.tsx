120c4d3bf69247021950fd5dc965f8c834b26443
Dan Bergelt <dan@danbergelt.com>
2020-03-09 16:07:59 -0400

    :art: textblocks refactored
    textblocks refactored to use scss modules

--- src/components/home/TextBlocks.tsx
+++ src/components/home/TextBlocks.tsx
@@ -1,53 +1,63 @@
 import React from 'react';
 import { FiEdit3, FiTarget, FiCalendar } from 'react-icons/fi';
+import styles from './TextBlocks.module.scss';
+import Flex from '../lib/Flex';
+import { useWindowSize } from 'react-use';
 
 export const TextBlocks: React.FC = () => {
-  const strokeWidth = { strokeWidth: 1.25 };
+  const STROKE_WIDTH = '1.25px';
+
+  const { width } = useWindowSize();
 
   return (
-    <article className='textblocks-container' id='about'>
-      <header className='textblocks-title'>
+    <Flex align='center' fd='column' cn={styles.container}>
+      <h2 className={styles.title}>
         Spotter is a lifting-focused fitness pal that helps you...
-      </header>
-      <section className='textblocks'>
-        <div className='textblock'>
-          <div className='textblock-icon track'>
-            <FiEdit3 style={strokeWidth} />
+      </h2>
+      <Flex
+        fd={width <= 800 ? 'column' : undefined}
+        align={width <= 800 ? 'center' : undefined}
+        justify='space-between'
+        cn={styles.blocks}
+      >
+        <div className={styles.block}>
+          <div className={`${styles.icon} ${styles.track}`}>
+            <FiEdit3 strokeWidth={STROKE_WIDTH} />
           </div>
-          <p className='textblock-title'>Track your lifts</p>
-          <p className='textblock-content'>
+          <p className={styles.blockTitle}>Track your lifts</p>
+          <p className={styles.content}>
             Our dashboard{' '}
-            <span className='textblock-highlight'>
+            <span className={styles.highlight}>
               automates your fitness journey
             </span>{' '}
             and makes tracking your lifts fun and easy.
           </p>
         </div>
-        <div className='textblock'>
-          <div className='textblock-icon bests'>
-            <FiTarget style={strokeWidth} />
+        <div className={styles.block}>
+          <div className={`${styles.icon} ${styles.bests}`}>
+            <FiTarget strokeWidth={STROKE_WIDTH} />
           </div>
-          <p className='textblock-title'>Log your personal bests</p>
-          <p className='textblock-content'>
+          <p className={styles.blockTitle}>Log your personal bests</p>
+          <p className={styles.content}>
             Save the exercises you want tracked, and we&#39;ll{' '}
-            <span className='textblock-highlight'>
+            <span className={styles.highlight}>
               calculate your PRs automatically.
             </span>
           </p>
         </div>
-        <div className='textblock'>
-          <div className='textblock-icon scale'>
-            <FiCalendar style={strokeWidth} />
+        <div className={styles.block}>
+          <div className={`${styles.icon} ${styles.scale}`}>
+            <FiCalendar strokeWidth={STROKE_WIDTH} />
           </div>
-          <p className='textblock-title'>Stay organized</p>
-          <p className='textblock-content'>
+          <p className={styles.blockTitle}>Stay organized</p>
+          <p className={styles.content}>
             Spreadsheets are unwieldy.{' '}
-            <span className='textblock-highlight'>Access, view, and edit</span>{' '}
-            any of your workouts, with ease.
+            <span className={styles.highlight}>Access, view, and edit</span> any
+            of your workouts, with ease.
           </p>
         </div>
-      </section>
-    </article>
+      </Flex>
+    </Flex>
   );
 };
 
