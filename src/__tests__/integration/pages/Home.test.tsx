864be645cd53643d02c19a03d086fbe775d9c2fe
Dan Bergelt <dan@danbergelt.com>
2020-03-10 17:24:48 -0400

    :art: features component refactored
    features component refactored --> consider moving static assets to S3 bucket if bundle size grows too large

--- src/__tests__/integration/pages/Home.test.tsx
+++ src/__tests__/integration/pages/Home.test.tsx
@@ -2,9 +2,22 @@ import React from 'react';
 import wrapper from 'src/__testUtils__/wrapper';
 import { reducer } from 'src/reducers';
 import Home from 'src/pages/Home';
+import mqPolyfill from 'mq-polyfill';
 import { fireEvent } from '@testing-library/dom';
 
 describe('home page tests', () => {
+  beforeAll(() => {
+    mqPolyfill(window);
+    window.resizeTo = function resizeTo(width, height) {
+      Object.assign(this, {
+        innerWidth: width,
+        innerHeight: height,
+        outerWidth: width,
+        outerHeight: height
+      }).dispatchEvent(new this.Event('resize'));
+    };
+  });
+
   test('bottom cta link works', () => {
     const { getByTestId, history } = wrapper(reducer, <Home />);
 
@@ -37,5 +50,54 @@ describe('home page tests', () => {
     fireEvent.click(getByTestId(/alt/i));
 
     expect(history.location.pathname).toEqual('/login');
+  });
+
+  test('features renders tabs when width > 1000', () => {
+    window.resizeTo(1001, 1000);
+    const { getByTestId } = wrapper(reducer, <Home />);
+
+    getByTestId(/tab/i);
+  });
+
+  test('features renders select when width <= 1000', () => {
+    window.resizeTo(1000, 1000);
+    const { getByTestId } = wrapper(reducer, <Home />);
+
+    getByTestId(/select/i);
+  });
+
+  test('hover selection works', () => {
+    window.resizeTo(1001, 1000);
+    const { getByText, getByAltText } = wrapper(reducer, <Home />);
+
+    getByAltText(/week view/i);
+
+    fireEvent.mouseOver(getByText(/month view/i));
+
+    getByAltText(/month view/i);
+
+    fireEvent.mouseOver(getByText(/PR Tracking/i));
+
+    getByAltText(/pr tracking/i);
+  });
+
+  test('select works', () => {
+    window.resizeTo(999, 1000);
+
+    const { getByTestId, getByAltText } = wrapper(reducer, <Home />);
+
+    getByAltText(/week view/i);
+
+    fireEvent.change(getByTestId(/select/i), {
+      target: { value: 'Month View' }
+    });
+
+    getByAltText(/month view/i);
+
+    fireEvent.change(getByTestId(/select/i), {
+      target: { value: 'PR Tracking' }
+    });
+
+    getByAltText(/pr tracking/i);
   });
 });
--- src/components/home/Features.tsx
+++ src/components/home/Features.tsx
@@ -1,35 +1,116 @@
 import React, { useState } from 'react';
-import weeklyview from '../../assets/weeklyview.png';
-import monthlyview from '../../assets/monthlyview.png';
-import prpage from '../../assets/prpage.png';
-import FeatureOptionsLarge from './FeatureOptionsLarge';
-import FeatureOptions from './FeatureOptions';
 import { useWindowSize } from 'react-use';
+import Flex from '../lib/Flex';
+import styles from './Features.module.scss';
 
-const Features: React.FC = () => {
-  const [selected, setSelected] = useState<string>('Week View');
+/*== Features =====================================================
 
-  const { width }: { width: number } = useWindowSize();
+Features component. Currently includes weekly view, monthly view,
+and PR's page. Covers the various features of spotter (will scale up
+as app scales up)
 
-  const setImage = (): string => {
-    if (selected === 'Week View') {
-      return weeklyview;
-    } else if (selected === 'Month View') {
-      return monthlyview;
+On desktop, reveal screenshots by hover. On mobile, reveal with a
+select element.
+
+*/
+
+interface Props {
+  images: string[];
+}
+
+const Features: React.FC<Props> = ({ images }) => {
+  // image keys
+  const WEEK_VIEW = 'Week View';
+  const MONTH_VIEW = 'Month View';
+  const PR_PAGE = 'PR Tracking';
+
+  // currently selected screenshot
+  const [selected, setSelected] = useState<string>(WEEK_VIEW);
+
+  // window width for inline styles
+  const { width } = useWindowSize();
+
+  // util function to dynamically set classname
+  const setClass = (el: string): string => {
+    if (selected === el) {
+      return styles.selected;
+    }
+    return styles.feature;
+  };
+
+  // render img based on img key
+  const setImg = (): string => {
+    if (selected === WEEK_VIEW) {
+      return images[0];
+    } else if (selected === MONTH_VIEW) {
+      return images[1];
     } else {
-      return prpage;
+      return images[2];
     }
   };
 
+  // wrapper component that surrounds both select and tabs
+  const Wrapper: React.FC = ({ children }) => {
+    return (
+      <>
+        <Flex
+          cn={styles.container}
+          justify='center'
+          align={width <= 1000 ? 'center' : undefined}
+          fd={width <= 1000 ? 'column' : undefined}
+        >
+          <section className={styles.tabs}>
+            <h2 className={styles.title}>Features</h2>
+            {children}
+          </section>
+          <img src={setImg()} className={styles.img} alt={selected} />;
+        </Flex>
+      </>
+    );
+  };
+
+  // if width <= 1000, render a select
+  if (width <= 1000) {
+    return (
+      <Wrapper>
+        <select
+          data-testid='select'
+          name='Feature Tabs'
+          value={selected}
+          onChange={(e): void => setSelected(e.target.value)}
+          className={styles.select}
+        >
+          <option>Week View</option>
+          <option>Month View</option>
+          <option>PR Tracking</option>
+        </select>
+      </Wrapper>
+    );
+  }
+
+  // otherwise render the hoverable tabs
   return (
-    <article className='features-container'>
-      {width <= 1000 ? (
-        <FeatureOptions selected={selected} setSelected={setSelected} />
-      ) : (
-        <FeatureOptionsLarge selected={selected} setSelected={setSelected} />
-      )}
-      <img className='features-img' src={setImage()} alt='Weekly View' />
-    </article>
+    <Wrapper>
+      <p
+        data-testid='tab'
+        onMouseOver={(): void => setSelected(WEEK_VIEW)}
+        className={setClass(WEEK_VIEW)}
+      >
+        Week View
+      </p>
+      <p
+        onMouseOver={(): void => setSelected(MONTH_VIEW)}
+        className={setClass(MONTH_VIEW)}
+      >
+        Month View
+      </p>
+      <p
+        onMouseOver={(): void => setSelected(PR_PAGE)}
+        className={setClass(PR_PAGE)}
+      >
+        PR Tracking
+      </p>
+    </Wrapper>
   );
 };
 
