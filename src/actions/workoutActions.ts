58a5adc811a4ee5a2ee2c213f985bec6100ca5f8
Dan Bergelt <dan@danbergelt.com>
2020-03-16 21:42:11 -0400

    :white_check_mark: removed dead tests
    removed dead tests

--- src/actions/workoutActions.ts
+++ src/actions/workoutActions.ts
@@ -39,7 +39,6 @@ export const resetExerciseFormAction: TResetExerciseForm = handleReset => {
 // adds a new exercise to the current workout
 type TAddExercise = (values: Exercise) => { type: string; payload: Exercise };
 export const addExerciseAction: TAddExercise = values => {
-  console.log(values);
   return { type: ADD_EXERCISE, payload: values };
 };
 
