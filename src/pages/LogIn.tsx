c687bfd41d339cd51932bb847558b7697f8c121f
Dan Bergelt <dan@danbergelt.com>
2020-03-25 17:33:35 -0400

    :art: auth pages (refactored and tested)
    moved some non-DRY props to the child auth form, wrote tests for auth pages to make sure that appropriate content renders depending on the context

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,35 +1,25 @@
-import React, { useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import { ReactComponent as LogInLogo } from '../assets/spotter_login.svg';
 import AuthForm from '../components/auth/AuthForm';
-import { useDispatch } from 'react-redux';
-import { addTokenAction } from 'src/actions/globalActions';
 import { Helmet } from 'react-helmet-async';
 import { logInQuery } from 'src/utils/queries';
 import styles from './AuthPage.module.scss';
 
+/*== Log in page =====================================================
+
+This is the log in page for unauth'd users. Utilizes a generic auth
+form that accepts slightly different props depending on context (log 
+in or sign up)
+
+*/
+
 const LogIn: React.FC = () => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-
-  const addToken = useCallback(
-    (t: string): void => {
-      dispatch(addTokenAction(t));
-    },
-    [dispatch]
-  );
-
   return (
     <>
       <Helmet>
         <title>Log In | Spotter</title>
       </Helmet>
-      <AuthForm
-        history={history}
-        api={logInQuery}
-        action='Log In'
-        addToken={addToken}
-      >
+      <AuthForm api={logInQuery} action='Log In'>
         <LogInLogo
           data-testid='login-img'
           role='img'
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,35 +1,25 @@
-import React, { useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import { ReactComponent as SignUpLogo } from '../assets/spotter_register.svg';
 import AuthForm from '../components/auth/AuthForm';
-import { useDispatch } from 'react-redux';
-import { addTokenAction } from 'src/actions/globalActions';
 import { Helmet } from 'react-helmet-async';
 import { signUpQuery } from 'src/utils/queries';
 import styles from './AuthPage.module.scss';
 
+/*== Sign up page =====================================================
+
+This is the sign up page for unauth'd users. Utilizes a generic auth
+form that accepts slightly different props depending on context (log 
+in or sign up)
+
+*/
+
 const SignUp: React.FC = () => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-
-  const addToken = useCallback(
-    (t: string): void => {
-      dispatch(addTokenAction(t));
-    },
-    [dispatch]
-  );
-
   return (
     <>
       <Helmet>
         <title>Sign Up | Spotter</title>
       </Helmet>
-      <AuthForm
-        history={history}
-        api={signUpQuery}
-        action='Sign Up'
-        addToken={addToken}
-      >
+      <AuthForm api={signUpQuery} action='Sign Up'>
         <SignUpLogo
           data-testid='signup-img'
           role='img'
